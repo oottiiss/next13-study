bf54b0f735abfb838b12b91d8abfa5eeb37bcebe
osh <dhtpgus7@gmail.com>
2023-08-15 18:10:49 +0900

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           <div>2</div>
           <div>3</div>
           <div>4</div>
-          <div>51</div>
+          <div>5</div>
         </Grid>
       </Box>
     </>
